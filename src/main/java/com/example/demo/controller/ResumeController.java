e63a2ce1627d571665d587b158a52796fb79d077
ajay109458 <ajay.yadav109458@gmail.com>
2019-11-18 22:43:48 +0530

    resume builder

--- src/main/java/com/example/demo/controller/ResumeController.java
+++ src/main/java/com/example/demo/controller/ResumeController.java
@@ -6,46 +6,57 @@ import javax.validation.Valid;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Resume;
 import com.example.demo.repository.ResumeRepository;
 
 @RestController
-@RequestMapping("/resume")
+@RequestMapping("/resumes")
 public class ResumeController {
 
 	@Autowired
 	ResumeRepository resumeRepository;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
 	public List<Resume> getAllResume() {
 		return resumeRepository.findAll();
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
 	public Resume getResumeById(@PathVariable("id") ObjectId id) {
 		return resumeRepository.findBy_id(id);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.OK)
 	public void modifyResumeById(@PathVariable("id") ObjectId id, @Valid @RequestBody Resume resume) {
 		resume.set_id(id);
 		resumeRepository.save(resume);
 	}
 
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Resume createResume(@Valid @RequestBody Resume resume) {
 		resume.set_id(ObjectId.get());
 		resumeRepository.save(resume);
 		return resume;
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping("{id}")
+	@ResponseStatus
 	public void deleteResume(@PathVariable ObjectId id) {
 		resumeRepository.delete(resumeRepository.findBy_id(id));
 	}
